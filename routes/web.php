d1bd365ed038966b8b8da9007515c2551160350a
Saad5400 <sdbtwa@gmail.com>
2025-01-03 20:22:04 +0300

    auth middleware

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', fn() => redirect()->route('request.create'));
 Route::get('login', \App\Livewire\Login::class)->name('login');
 Route::get('register', \App\Livewire\Register::class)->name('register');
 
-Route::get('request/create', \App\Livewire\Request\Create::class)->name('request.create');
-Route::get('request/edit', \App\Livewire\Request\Edit::class)->name('request.edit');
+Route::get('request/create', \App\Livewire\Request\Create::class)->name('request.create')->middleware('auth');
+Route::get('request/edit', \App\Livewire\Request\Edit::class)->name('request.edit')->middleware('auth');
