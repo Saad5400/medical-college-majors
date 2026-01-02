4c6007195c1c0b90b8fa74bec24eaca67f266674
Saad5400 <sdbtwa@gmail.com>
2026-01-02 21:56:34 +0300

    dd

--- app/Filament/Resources/MajorResource/Pages/ListMajors.php
+++ app/Filament/Resources/MajorResource/Pages/ListMajors.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\MajorResource\Pages;
 
 use App\Filament\Resources\MajorResource;
+use App\Models\User;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListMajors extends ListRecords
@@ -14,6 +17,40 @@ class ListMajors extends ListRecords
     {
         return [
             CreateAction::make(),
+            Action::make('distribute')
+                ->label('توزيع الطلاب على المسارات')
+                ->action(function () {
+                    // Reset all users' major_id
+                    User::query()->update(['major_id' => null]);
+
+                    $users = User::query()
+                        ->orderBy('gpa', 'desc')
+                        ->get();
+
+                    /** @var User $user */
+                    foreach ($users as $user) {
+                        if ($user->registrationRequests()->count() === 0) {
+                            continue;
+                        }
+
+                        $registrationRequest = $user->registrationRequests()->latest()->first();
+                        $majors = $registrationRequest->majors()->orderByPivot('sort')->get();
+
+                        foreach ($majors as $major) {
+                            if ($major->users()->count() < $major->max_users) {
+                                $user->major()->associate($major);
+                                $user->save();
+
+                                break;
+                            }
+                        }
+                    }
+
+                    Notification::make()
+                        ->title('تم توزيع الطلاب على المسارات')
+                        ->success()
+                        ->send();
+                }),
         ];
     }
 }
