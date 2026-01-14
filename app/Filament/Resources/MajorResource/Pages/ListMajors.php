94455eaac91f2d4853267a8c77df1db7398d587b
Saad Batwa <86385454+Saad5400@users.noreply.github.com>
2026-01-14 11:23:57 +0300

    Update ListMajors.php

--- app/Filament/Resources/MajorResource/Pages/ListMajors.php
+++ app/Filament/Resources/MajorResource/Pages/ListMajors.php
@@ -23,7 +23,7 @@ class ListMajors extends ListRecords
             ExportAction::make()
                 ->exporter(UserExporter::class)
                 ->label('تصدير الطلاب')
-                ->modifyQueryUsing(fn () => User::query()->whereDoesntHave('roles')),
+                ->modifyQueryUsing(fn () => User::query()->whereDoesntHave('roles')->orderBy('major_id')),
             Action::make('distribute')
                 ->label('توزيع الطلاب على المسارات')
                 ->action(function () {
